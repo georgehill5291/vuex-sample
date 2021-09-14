21e763f5c25e9c008fdcc6254f82ff0e5739de88
georgehill5291 <hieuden0@gmail.com>
2021-09-14 23:36:29 +0700

    Revert "Revert "Revert "update env"""
    This reverts commit f167e184bdaa3cb5b9e77b3616dd848872a4ce90.

--- src/util/constUtil.js
+++ src/util/constUtil.js
@@ -1,6 +1,4 @@
-export const apiUrl =
-    process.env.VUE_APP_MY_ENV_VARIABLE !== 'production'
-        ? 'http://localhost:4000/api'
-        : process.env.VUE_APP_PROD_API_URL
-
-export const authName = 'gauth'
+export default {
+    AUTH_TOKEN_KEY: 'gauth',
+    API_URL: 'http://localhost:4000/api'
+}
