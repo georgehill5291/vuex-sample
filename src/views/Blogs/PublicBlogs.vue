8cf9d2dd19906c6d2a65a0723ef63f0e9e6f3e28
georgehill5291 <hieuden0@gmail.com>
2021-09-19 23:18:19 +0700

    update alert

--- src/views/Blogs/PublicBlogs.vue
+++ src/views/Blogs/PublicBlogs.vue
@@ -39,10 +39,10 @@ export default {
         copyClipboard(blogId) {
             const full = location.protocol + '//' + location.host
             var blogUrl = `${full}/blogs/${blogId}`
-            navigator.clipboard.writeText(blogUrl)
             navigator.clipboard.writeText('<empty clipboard>').then(
                 function() {
                     alert('Copied the text: ' + blogUrl)
+                    navigator.clipboard.writeText(blogUrl)
                 },
                 function() {
                     alert('Copied the text failed')
