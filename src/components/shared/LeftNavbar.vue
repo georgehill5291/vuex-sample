40737a6da0447e5feca17915ec3c4dc2550cf006
georgehill5291 <hieuden0@gmail.com>
2021-09-21 08:08:13 +0700

    update left-sidebar

--- src/components/shared/LeftNavbar.vue
+++ src/components/shared/LeftNavbar.vue
@@ -1,8 +1,10 @@
 <template>
     <div class="left-sidebar bg-dark">
-        Left nav bar
-
-        <ul class="navbar-nav">
+        <ul class="navbar-nav bg-dark">
+            <li class="nav-item active text-left p-2 left-item">
+                <span class="menu-nav" @click="toggleNavigation">&#9776;</span>
+                Navigation
+            </li>
             <li class="nav-item active text-left p-2 left-item">
                 Hi {{ username }}
             </li>
@@ -10,8 +12,13 @@
                 <router-link to="/portal/sample-vuex">Sample Vuex</router-link>
             </li>
             <li class="nav-item text-left p-2 left-item">
-                <router-link class="nav-link" to="/portal/blog-listing"
-                    >Blog</router-link
+                <router-link class="nav-link" to="/portal/blog-listing">
+                    <img
+                        src="/assets/images/ic_faq.svg"
+                        alt="menu"
+                        class="mCS_img_loaded"
+                    />
+                    <span>Blog</span></router-link
                 >
             </li>
         </ul>
@@ -20,37 +27,58 @@
 
 <script>
 import { mapGetters } from 'vuex'
+import $ from 'jquery'
 export default {
     name: 'LeftNavbar',
-    computed: mapGetters(['username'])
+    computed: mapGetters(['username']),
+    methods: {
+        toggleNavigation() {
+            $('div.left-sidebar').toggleClass('open')
+            $('.portal-container').toggleClass('open')
+        }
+    }
 }
 </script>
 
-<style>
+<style lang="scss">
 .left-sidebar {
-    position: -webkit-sticky;
-    position: sticky;
+    background: #232343;
+    position: fixed;
+    left: 0;
     top: 0;
-    padding-top: 50px;
-    width: 250px;
-    height: 100vh;
-}
+    padding-top: 70px;
+    padding-bottom: 70px;
+    height: 100%;
+    width: 80px;
+    float: left;
+    box-shadow: none;
+    transition: all 0.1s linear;
 
-.left-item {
-    border-bottom: 1px solid white;
-    color: white;
-}
-
-.left-item a {
-    color: white;
-}
-
-/* @media (max-width: 768px) {
-    .left-sidebar {
-        position: absolute;
-        width: 100vw;
-        height: 100vh;
-        display: none;
+    .left-item {
+        border-bottom: 1px solid white;
+        color: white;
     }
-} */
+
+    .left-item a {
+        color: white;
+    }
+
+    .left-item img.mCS_img_loaded {
+        vertical-align: middle;
+        position: relative;
+        top: -2px;
+        margin-right: 10px;
+        width: 20px;
+    }
+}
+
+.left-sidebar.open {
+    width: 200px;
+}
+
+@media (max-width: 768px) {
+    .left-sidebar {
+        height: 100vh;
+    }
+}
 </style>
--- src/views/template/Portal.vue
+++ src/views/template/Portal.vue
@@ -7,12 +7,25 @@
     </div>
 </template>
 
-<style scoped>
+<style scoped lang="scss">
 .portal {
     display: flex;
 }
+
 .portal-container {
     margin-top: 60px;
     flex: 1;
+
+    float: left;
+    // width: 100%;
+    padding: 0px 0px 0px 80px;
+    min-height: 500px;
+    background: #fafafb;
+}
+
+@media (min-width: 768px) {
+    .portal-container.open {
+        padding: 0px 0px 0px 200px;
+    }
 }
 </style>
